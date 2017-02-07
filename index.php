0a36551b77f970f2bf472b58ae1ec6ced64e5495
Travis Peterson <petetrav42@gmail.com>
2017-02-07 17:14:27 -0600

    Added random quote

--- index.php
+++ index.php
@@ -10,17 +10,22 @@
 </head>
 
 <body>
-<h1>Travis Peterson</h1>
-<img src="/images/me.jpg">
-<p>My name is Travis Peterson and I currently live in San Antonio Texas.  I currently work for USAA where I lead and
-    develop mainly Java based applications on internal systems. I am working on my Information Management Systems Masters
-    degree here at the Harvard Extension School and this is my last semester.  On my spare time I have done web development
-    in some form or another.  I have worked in PHP before but am looking to take my PHP knowledge and skills to the next
-    level and so that is what I am looking to get out of this class.  I am happily married and have 3 kids (2 daughters
-    and a boy) and so besides work and homework most of my time is spent hanging out with my family.
-</p>
-<?php
-    echo $randomQuotes;
-?>
+<div id="mainContainer">
+    <h1>Travis Peterson</h1>
+    <img src="images/me.jpg">
+    <p class="intro">My name is Travis Peterson and I currently live in San Antonio Texas.  I currently work for USAA where I lead and
+        develop mainly Java based applications on internal systems. I am working on my Information Management Systems Masters
+        degree here at the Harvard Extension School and this is my last semester.  On my spare time I have done web development
+        in some form or another.  I have worked in PHP before but am looking to take my PHP knowledge and skills to the next
+        level and so that is what I am looking to get out of this class.  I am happily married and have 3 kids (2 daughters
+        and a boy) and so besides work and homework most of my time is spent hanging out with my family.
+    </p>
+    <div class="randomQuote">
+        <h2>Random Quote</h2>
+        <?php
+            echo $randomQuotes;
+        ?>
+    </div>
+</div>
 </body>
 </html>
--- randomQuote.php
+++ randomQuote.php
@@ -2,9 +2,9 @@
 
 $quotes =
 [
-    'This is the first quote',
-    'This is the second quote',
-    'This is the third quote',
+    'We’re going to turn this team around 360 degrees. – Jason Kidd',
+    'Baseball is 90% mental. The other half is physical. – Yogi Berra',
+    'I owe a lot to my parents, especially my mother and father. – Greg Norman',
 ];
 
 $randomQuotes = $quotes[array_rand($quotes)];
