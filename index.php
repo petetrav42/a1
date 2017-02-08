4f1f9f8d4ccad71de2f541b0cfc4af028c92071e
Travis Peterson <petetrav42@gmail.com>
2017-02-07 18:16:18 -0600

    Fixed html validation errors

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body>
 <div id="mainContainer">
     <h1>Travis Peterson</h1>
-    <img src="images/me.jpg">
+    <img src="/images/me.jpg" alt="Me">
     <p class="intro">My name is Travis Peterson and I currently live in San Antonio Texas.  I currently work for USAA where I lead and
         develop mainly Java based applications on internal systems. I am working on my Information Management Systems Masters
         degree here at the Harvard Extension School and this is my last semester.  On my spare time I have done web development
