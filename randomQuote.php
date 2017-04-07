2678e941455d392b43ebd5f44d8d3520e62fbfee
Travis Peterson <petetrav42@gmail.com>
2017-04-07 00:36:37 -0500

    4/6

---
+++ randomQuote.php
@@ -0,0 +1,10 @@
+<?php
+
+$quotes =
+[
+    'We’re going to turn this team around 360 degrees. – Jason Kidd',
+    'Baseball is 90% mental. The other half is physical. – Yogi Berra',
+    'I owe a lot to my parents, especially my mother and father. – Greg Norman',
+];
+
+$randomQuotes = $quotes[array_rand($quotes)];
