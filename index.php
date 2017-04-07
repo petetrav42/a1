1bfe598b69da0d46fc715ebce596d731c771f06e
Travis Peterson <petetrav42@gmail.com>
2017-04-06 23:56:11 -0500

    4/6 - test commit

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <div id="mainContainer">
     <h1>Travis Peterson</h1>
     <img src="/images/me.jpg" alt="Me">
-    <p class="intro">Hi, My name is Travis Peterson and I currently live in San Antonio Texas.  I currently work for USAA where I lead and
+    <p class="intro">My name is Travis Peterson and I currently live in San Antonio Texas.  I currently work for USAA where I lead and
         develop mainly Java based applications on internal systems. I am working on my Information Management Systems Masters
         degree here at the Harvard Extension School and this is my last semester.  On my spare time I have done web development
         in some form or another.  I have worked in PHP before but am looking to take my PHP knowledge and skills to the next
